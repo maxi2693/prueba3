5ea61368e42cf9bd861867083a09f9c4d9024135
maxi2693 <maximiliano_2693@hotmail.com>
2018-03-15 09:11:09 -0300

    Prueba3

---
+++ src/prueba3/Prueba3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba3;
+
+/**
+ *
+ * @author maxi
+ */
+public class Prueba3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+           System.out.println("hola");
+        // TODO code application logic here
+    }
+    
+}
